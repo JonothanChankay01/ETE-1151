d6dba30cbcf7a7300b0f966dad91641a4b7eed38
JonothanChankay01 <jochankay@cpp.edu>
2026-02-21 13:30:33 -0800

    lost

--- Homework/Homework-04/Part2.cpp
+++ Homework/Homework-04/Part2.cpp
@@ -2,13 +2,29 @@
 #include <iomanip>
 #include <string>
 
+//google says a leap year is divisisble by 4, but not by 100 unless it is also divisible by 400
 bool LeapYear(int year) {
-    if (year % 4 == 0 && year %100 != 0) {
-        return true;
-    } else if (year %4 ==0 && year %100 !=0 && year %400 == 0){
-        return true;
-    } else {
-        return false;
+    return (year %4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+int StartingDay (int y) { //This function came from google because I couldn't figure out a way to caculate the first day of each month
+   y-=1;
+   return (y + y/4 - y/100 + y/400 +1) % 7;
+
+}
+
+int main(){ 
+    int year;
+    std::cout<<"Enter a year: ";
+    std::cin>>year;
+
+    std::string months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    int NumberOfDays[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    //Changes 28 to 29 for february if it is a leap year
+    if(LeapYear(year)){
+        NumberOfDays[1]=29;
     }
 }
+
 
