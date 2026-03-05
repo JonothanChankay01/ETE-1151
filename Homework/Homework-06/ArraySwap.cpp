164562441780849ef808dc8196b82c27e5863e01
JonothanChankay01 <jochankay@cpp.edu>
2026-03-04 18:43:54 -0800

    HW 6

--- Homework/Homework-06/ArraySwap.cpp
+++ Homework/Homework-06/ArraySwap.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 
-int swap(){
-    
+int data [] = {1, 3 , 6, 7, 9};
+int data1[] = {5, 3, 3, 11};
 
+int swap(){
+
+    //holds the numbers from data[] in tempoerary while swapping the two
+    //can only swap the first four in data[] since there are only four array slots so i increments to 4
+    for (int i = 0; i < 4; i++){
+        int temporary = data[i];
+        data[i] = data1[i];
+        data1[i] = temporary;
+    }
 
 }
 
+int common(){
+    
+
+}
+
+
+
 int main(){
-    int data [] = {1, 3 , 6, 7, 9};
-    int data1[] = {5, 3, 3, 11};
     
 }
